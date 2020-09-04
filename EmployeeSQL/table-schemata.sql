44476a4240d801b1391dd11cca3668deccecf61c
Artem Bordetskiy <abordetskiy@gmail.com>
2020-09-04 13:45:10 -0500

    added comments to schema table-schemata file

--- EmployeeSQL/table-schemata.sql
+++ EmployeeSQL/table-schemata.sql
@@ -1,10 +1,12 @@
 --Table Schemata
+--tables created and imported from CSV files in same order to preserve data
+--titles selected first so as to no interfere with different name in employees table
 CREATE TABLE titles(
 	title_id VARCHAR(5) NOT NULL,
 	title VARCHAR(255) NOT NULL,
 	PRIMARY KEY(title_id)
 	);	
-
+--length of characters dependent on max character length of field in CSV files (within reason, otherwise 255)
 CREATE TABLE employees(
 	emp_no INTEGER NOT NULL,
 	emp_title_id VARCHAR(5) NOT NULL,
@@ -16,25 +18,25 @@ CREATE TABLE employees(
 	PRIMARY KEY(emp_no),
 	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
-
+--junction table for department names
 CREATE TABLE departments(
 	dept_no VARCHAR(4) NOT NULL,
 	dept_name VARCHAR(255) NOT NULL,
 	PRIMARY KEY(dept_no)
 );
-
+--junction table for department numbers
 CREATE TABLE dept_emp(
 	emp_no INTEGER NOT NULL,
 	dept_no VARCHAR(4) NOT NULL,
 	PRIMARY KEY(emp_no,dept_no)
 );
-
+--table of department managers
 CREATE TABLE dept_manager(
 	dept_no VARCHAR(4) NOT NULL,
 	emp_no INTEGER NOT NULL,
 	PRIMARY KEY(dept_no,emp_no)
 );
-
+--table of salaries
 CREATE TABLE salaries(
 	emp_no INTEGER NOT NULL,
 	salary DECIMAL NOT NULL,
